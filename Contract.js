303366aaf1b6a0125974e5e7f54d7b9ef9584554
joseherminiocollas <joseherminiocollas@goatstone.com>
2023-06-05 20:36:30 -0400

    Update Contract

--- Contract.js
+++ Contract.js
@@ -1,4 +1,9 @@
-const { ContractCreateFlow } = require('@hashgraph/sdk');
+const {
+  ContractCreateFlow,
+  ContractExecuteTransaction,
+  ContractFunctionParameters,
+  Hbar,
+} = require('@hashgraph/sdk');
 
 const Contract = async (client, bytecode) => {
   const createContract = new ContractCreateFlow()
@@ -7,8 +12,27 @@ const Contract = async (client, bytecode) => {
   const createContractTx = await createContract.execute(client);
   const createContractRx = await createContractTx.getReceipt(client);
   const contractId = createContractRx.contractId;
+  console.log(`contractId: ${contractId}`);
 
-  return contractId;
+  // Create NFT from precompile
+  const createToken = new ContractExecuteTransaction()
+    .setContractId(contractId)
+    .setGas(4000000) // Increase if revert
+    .setPayableAmount(50) // Increase if revert 1 50 100
+    .setFunction("createNft",
+      new ContractFunctionParameters()
+        .addString("Name") // NFT name
+        .addString("Symbol") // NFT symbol
+        .addString("memo") // NFT memo
+        .addUint32(250) // NFT max supply
+        .addUint32(7000000) // Expiration: Needs to be between 6999999 and 8000001
+    );
+  const createTokenTx = await createToken.execute(client);
+  const createTokenRx = await createTokenTx.getRecord(client);
+  const tokenIdSolidityAddr = createTokenRx.contractFunctionResult.getAddress(0);
+  const tokenId = AccountId.fromSolidityAddress(tokenIdSolidityAddr);
+  console.log(`tokenId: ${tokenId}`);
+
 }
 
 module.exports = Contract;
