619c4b907e3a5ef2e0b4a8e742546876428dddc8
joseherminiocollas <joseherminiocollas@goatstone.com>
2023-06-12 14:23:02 -0400

    Set up work flow with functions, confirm test with hashscan

---
+++ createNFT.js
@@ -0,0 +1,59 @@
+const {
+    AccountId,
+    ContractCreateFlow,
+    ContractExecuteTransaction,
+    ContractFunctionParameters,
+    Hbar,
+} = require('@hashgraph/sdk');
+
+const createNFT = async (client, contractId) => {
+    // const createContract = new ContractCreateFlow()
+    //   .setGas(4000000)
+    //   .setBytecode(bytecode);
+    // const createContractTx = await createContract.execute(client);
+    // const createContractRx = await createContractTx.getReceipt(client);
+    // const contractId = createContractRx.contractId;
+    // console.log(`contractId: ${contractId}`);
+    // return contractId;
+
+    // Create NFT from precompile
+    const createToken = new ContractExecuteTransaction()
+        .setContractId(contractId)
+        .setGas(4000000) // Increase if revert
+        .setPayableAmount(50) // Increase if revert 1 50 100
+        .setFunction("createNft",
+            new ContractFunctionParameters()
+                .addString("TEST name") // NFT name
+                .addString("TEST Symbol") // NFT symbol
+                .addString("TEST memo") // NFT memo
+                .addInt64(250) // NFT max supply
+                .addInt64(7000000) // Expiration: Needs to be between 6999999 and 8000001
+        );
+    const createTokenTx = await createToken.execute(client);
+    const createTokenRx = await createTokenTx.getRecord(client);
+    const tokenIdSolidityAddr = createTokenRx.contractFunctionResult.getAddress(0);
+    const tokenId = AccountId.fromSolidityAddress(tokenIdSolidityAddr);
+    console.log(`tokenId: ${tokenId}`);
+    return { tokenIdSolidityAddr, tokenId };
+    // // IPFS URI
+    // // metadata = "ipfs://bafyreie3ichmqul4xa7e6xcy34tylbuq2vf3gnjf7c55trg3b6xyjr4bku/metadata.json";
+    // metadata = 'bafybeih7peh6mbh63iwmx6n3jallwad3sfdh6sp7qfet7nxn6fedae5zd4'
+    // // Mint NFT
+    // const mintToken = new ContractExecuteTransaction()
+    //   .setContractId(contractId)
+    //   .setGas(4000000)
+    //   .setMaxTransactionFee(new Hbar(20)) //Use when HBAR is under 10 cents
+    //   .setFunction("mintNft",
+    //     new ContractFunctionParameters()
+    //       .addAddress(tokenIdSolidityAddr) // Token address
+    //       .addBytesArray([Buffer.from(metadata)]) // Metadata
+    //   );
+    // const mintTokenTx = await mintToken.execute(client);
+    // const mintTokenRx = await mintTokenTx.getRecord(client);
+    // const serial = mintTokenRx.contractFunctionResult.getInt64(0);
+    // console.log(`Minted NFT with serial: ${serial} \n`);
+
+    // return tokenId;
+}
+
+module.exports = createNFT;
---
+++ mintVariciteLogos.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const { AccountId,
+    PrivateKey,
+    Client,
+} = require('@hashgraph/sdk');
+require('dotenv').config({ path: __dirname + '/.env' });
+const contract = require('./contractNFT');
+const createNFT = require('./createNFT');
+const mintNFT = require('./mintNFT');
+
+const operatorKey = PrivateKey.fromString(process.env.PRIVATE_KEY);
+const operatorId = AccountId.fromString(process.env.ACCOUNT_ID);
+const client = Client.forTestnet().setOperator(operatorId, operatorKey);
+const bytecode = fs.readFileSync('./binaries/NFTCreator_sol_NFTCreator.bin');
+
+const func = (async () => {
+    const contractId = await contract(client, bytecode);
+    const { tokenId, tokenIdSolidityAddr } = await createNFT(client, contractId);
+    const serial = await mintNFT(client, contractId, tokenIdSolidityAddr)
+    console.log(`Contract created with ID:: ${contractId}`);
+    console.log(`X: ${tokenId}`);
+    console.log(`Y: ${serial}`);
+    process.exit(1)
+})()
