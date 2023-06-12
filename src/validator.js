d93e511e3f04ae66cc173345cef48f1e1ee78019
joseherminiocollas <joseherminiocollas@goatstone.com>
2023-06-12 18:41:46 -0400

    Add meta data and check with valicator

--- src/validator.js
+++ src/validator.js
@@ -1,11 +1,8 @@
 const { validator, defaultVersion } = require('@hashgraph/nft-utilities');
 
-const metadata = {
-    name: "Varicite Logo",
-    type: "image/svg+xml",
-    creator: "Varicite",
-    image: "https://bafybeih7peh6mbh63iwmx6n3jallwad3sfdh6sp7qfet7nxn6fedae5zd4.ipfs.dweb.link/logo.svg"
-};
+const logoA = require('./meta-data/logo-a.json')
+const logoB = require('./meta-data/logo-b.json')
+const logoC = require('./meta-data/logo-c.json')
+const logos = [logoA, logoB, logoC];
 
-const issues = validator( metadata , defaultVersion);
-console.log(issues)
+logos.forEach(logo => console.log(validator(logo, defaultVersion)))
