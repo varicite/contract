36e23c55a88cc1c1fb7da73159941eb219f0a2b3
joseherminiocollas <joseherminiocollas@goatstone.com>
2023-06-12 21:39:07 -0400

    Update run script with IPFS addresses

--- src/mintVariciteLogos.js
+++ src/mintVariciteLogos.js
@@ -15,9 +15,9 @@ const logoC = require('./meta-data/logo-c.json')
 // const logoMetaData = [logoA, logoB, logoC];
 // a series of IPFS URLS
 const logoMetaData = [
-	'bafybeiaf4rmlcjt3wv63bc56ttwkpo54wl6c3wwlhuurobygirmwzygpmm',
-	'bafybeifahcftlyfjt7n4ugzmwjnztkmskriczslojim5mrlojkpo5f4ufi',
-	'bafybeihnfi3adwgbzyllfpdvcmo3rlr3m2nkkkfceiheu2aoiohyoky7ta'
+	'ipfs://bafybeiamo6lhgvzth4ooktomqxn2vjtj22qviwcxop2istcmnthgnycsiq',
+	'ipfs://bafybeibq4wcqcbo5uzcyudyxnog5zih4iqbuhdhjbk62axvusfsh3ftzsq',
+	'ipfs://bafybeig4vtfh4aqticfcwyso2brpyy6a3zhd32i45qd5grbv7l7cesygx4'
 ];
 const operatorKey = PrivateKey.fromString(process.env.PRIVATE_KEY);
 const operatorId = AccountId.fromString(process.env.ACCOUNT_ID);
@@ -35,6 +35,7 @@ const func = (async () => {
 	);
 	const serial = await mintNFT(client, contractId, tokenIdSolidityAddr, logoMetaData[0])
 	const s2 = await mintNFT(client, contractId, tokenIdSolidityAddr, logoMetaData[1])
+	const s3 = await mintNFT(client, contractId, tokenIdSolidityAddr, logoMetaData[2])
 	// console.log(`serial: ${serial}`);
 	// logoMetaData.forEach(async metaData => {
 	// 		console.log(`serial `);
@@ -44,7 +45,7 @@ const func = (async () => {
 	// const serial = await mintNFT(client, contractId, tokenIdSolidityAddr)
 	console.log(
 		`ContractID : ${contractId} : tokenId: ${tokenId} : serial:  ${serial} `,
-		`serial: ${s2}`
+		`serial: ${s2}, ${s3}`
 	);
 	process.exit(1)
 })()
