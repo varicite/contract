f7d03781af31559142b1ab4898fe711918cf75a4
joseherminiocollas <joseherminiocollas@goatstone.com>
2023-06-23 09:43:03 -0400

    Send contractId in argument

--- src/fobs/spiral/mint.js
+++ src/fobs/spiral/mint.js
@@ -1,7 +1,6 @@
 const fs = require('fs');
 const { AccountId, PrivateKey, Client, } = require('@hashgraph/sdk');
 require('dotenv').config({ path: '../../../.env' });
-const contract = require('../../contractNFT');
 const createNFT = require('../../createNFT');
 const mintNFT = require('../../mintNFT');
 
@@ -12,8 +11,7 @@ const operatorId = AccountId.fromString(process.env.ACCOUNT_ID);
 const client = Client.forTestnet().setOperator(operatorId, operatorKey);
 const bytecode = fs.readFileSync('../../../binaries/NFTCreator_sol_NFTCreator.bin');
 
-const mint = async () => {
-    const contractId = await contract(client, bytecode);
+const mint = async (contractId) => {
     const { tokenId, tokenIdSolidityAddr } = await createNFT(
         client,
         contractId,
@@ -26,4 +24,6 @@ const mint = async () => {
     console.log(outMessage);
     process.exit(1)
 };
-mint();
+
+const contractId1 ='0.0.14971695' 
+mint(contractId1);
